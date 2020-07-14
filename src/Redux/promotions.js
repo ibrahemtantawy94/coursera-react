812dcad23e3e2de8026ac9162212c5c74ea28c05
ibrahem-tantawy94 <ibrahem.tantawy94@gmail.com>
2020-07-14 11:16:56 +0200

    + adding fetch , fetching data from json-server

--- src/Redux/promotions.js
+++ src/Redux/promotions.js
@@ -1,13 +1,18 @@
+import * as ActionTypes from './ActionTypes';
 import { PROMOTIONS } from '../shared/promotions';
 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (state  = { isLoading: true, errMess: null, promotions:[]}, action) => {
     switch (action.type) {
-        // case value:
-        //     return;
-        //     break;
-    
+        case ActionTypes.ADD_PROMOS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
+
+        case ActionTypes.PROMOS_LOADING:
+            return {...state, isLoading: true, errMess: null, promotions: []}
+
+        case ActionTypes.PROMOS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
         default:
             return state;
-        break;
     }
-}
+};
